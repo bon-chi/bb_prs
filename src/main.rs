998c839103fb0caf3a278eb524ec03773603dc5d
Koji Ota <afterjnih@gmail.com>
2016-10-04 18:05:36 +0900

    use env

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rustbox;
 extern crate hyper;
 
+use std::env;
 use std::error::Error;
 use std::default::Default;
 
@@ -9,8 +10,11 @@ use rustbox::Key;
 
 use hyper::client::Client;
 use std::io::Read;
-// use hyper::Client;
 use hyper::header::{Headers, Authorization, Basic};
+
+const BITBUCKET_API_URL: &'static str = "BITBUCKET_API_URL";
+const BITBUCKET_USERNAME: &'static str = "BITBUCKET_USERNAME";
+const BITBUCKET_PASSWORD: &'static str = "BITBUCKET_PASSWORD";
 
 fn main() {
     send_request();
@@ -62,24 +66,24 @@ fn main() {
 }
 
 fn send_request() {
-    let client = Client::new();
-    let authorization = Authorization(Basic {
-        username: "".to_string(),
-        password: Some("".to_string()),
-    });
-    let mut headers = Headers::new();
-    headers.set(authorization);
-    let mut res = client.get("")
-        .headers(headers)
-        .send()
-        .unwrap();
-    // let mut body = String::new();
-    // res.read_to_string(&mut body).unwrap();
-
-    // println!("{}", body);
-    let mut result = String::new();
-    res.read_to_string(&mut result);
-    println!("{:?}", result);
-    // println!("{:?}", res.chars());
-
+    if let Ok(api_url) = env::var(BITBUCKET_API_URL) {
+        if let Ok(user_name) = env::var(BITBUCKET_USERNAME) {
+            if let Ok(password) = env::var(BITBUCKET_PASSWORD) {
+                let client = Client::new();
+                let authorization = Authorization(Basic {
+                    username: user_name,
+                    password: Some(password),
+                });
+                let mut headers = Headers::new();
+                headers.set(authorization);
+                let mut res = client.get(api_url.as_str())
+                    .headers(headers)
+                    .send()
+                    .unwrap();
+                let mut result = String::new();
+                res.read_to_string(&mut result);
+                println!("{:?}", result);
+            }
+        }
+    }
 }
